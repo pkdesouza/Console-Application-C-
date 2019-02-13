4e98bf408ffc9d6e41ed3424b38015a8ab9b11ee
Patrick de Souza Ferreira <32820622+pkdesouza@users.noreply.github.com>
2019-02-13 18:52:48 -0200

    Add files via upload

---
+++ Operations/Retorno.cs
@@ -0,0 +1,8 @@
+﻿namespace Dominio
+{
+    public class Retorno
+    {
+        public bool Sucesso { get; set; } = true;
+        public string Mensagem { get; set; } = "Operação realizada";
+    }
+}
