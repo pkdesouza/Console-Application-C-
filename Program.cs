3ed424f90897a6e9d35457cd696fa68a849b4f17
Patrick de Souza Ferreira <32820622+pkdesouza@users.noreply.github.com>
2019-02-13 18:51:18 -0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Dominio;
+using static System.Console;
+using static System.Convert;
+using static Apresentacao.Operacao;
+
+namespace Apresentacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    RecuperarDados();
+                    int opcao = int.MaxValue;
+                    while (opcao != 0)
+                    {
+                        WriteLine("Operações: ");
+                        WriteLine("1 - Realizar Pagamento");
+                        WriteLine("2 - Realizar Deposito");
+                        WriteLine("3 - Consultar Saldo");
+                        WriteLine("4 - Exibir Extrato");
+                        WriteLine("0 - Sair");
+                        Write("Escolha a operação: ");
+                        opcao = ToInt32(ReadLine());
+                        switch (opcao)
+                        {
+                            case 1: RealizarPagamento(); break;
+                            case 2: RealizarDeposito(); break;
+                            case 3: Saldo(); break;
+                            case 4: ExibirExtrato(); break;
+                            default: break;
+                        }
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Clear();
+                    Erro.Registrar(e);
+                    WriteLine($"Falha de conversão!\nPossível causa do erro : {e.Message}\nPressione a tecla enter para prosseguir...");
+                    throw;
+                }
+                catch (OverflowException e)
+                {
+                    Clear();
+                    WriteLine($"Falha na operação com números!\nPossível causa do erro : {e.Message}\nPressione a tecla enter para prosseguir...");
+                    throw;
+                }
+                catch (Exception e) when(e.Message.ToLower().Contains("cod:"))
+                {
+                    Clear();
+                    WriteLine($"Erro do sistema!\nPossível causa do erro : {e.Message}\nPressione a tecla enter para prosseguir...");
+                    throw;
+                }
+                catch (Exception e)
+                {
+                    Clear();
+                    WriteLine($"Erro durante a operação!\nPossível causa do erro : {e.Message}\nPressione a tecla enter para prosseguir...");
+                    throw;
+                }
+            }
+            catch (Exception e)
+            {
+                ReadLine();
+                Clear();
+                Main(new string[] { });
+            }
+        }
+    }
+}
